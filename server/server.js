cbe6a64b4ed8f7b0bd88cae7e72b18274ad5de7f
Lorena Morales <Lorenarm.999@gmail.com>
2022-11-29 21:15:22 -0700

    routes

--- server/server.js
+++ server/server.js
@@ -1,42 +1,28 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-
+const express = require("express");
+const tarotData = require('../tarot-images.json');
+const tarotImages = require('../cards');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.get("/", (req,res) => {
+res.json(tarotData)
+})
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+router.get('/images/:imageName', function(req, res) {
+  var image = req.params['imageName'];
+  res.readFile(image, 'utf8', function(err, data){
+    if(err){
+      res.end(404);
+    }
+    res.send(data)    
+  });
 });
 
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
-  
-// Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
- 
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});
