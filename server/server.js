34efb672712cc0ffbbfc5e7b3f0ca30dc6a69ee0
Lorena Morales <Lorenarm.999@gmail.com>
2022-12-01 03:26:22 -0700

    finising API will keep working on it in the morning

--- server/server.js
+++ server/server.js
@@ -10,17 +10,32 @@ app.get("/", (req,res) => {
 res.json(tarotData)
 })
 
-app.get("/:cardname", (req,res) => {
-  const cardname = req.params.cardname
-  for (let index = 0; index < tarotData.length; index++) {
-    const tarotCard = tarotData[index];
-    if (tarotCard != cardname) {
-      return tarotCard
-    } 
-    res.json(tarotCard)
-  }
+app.get("/:cardNumber", (req,res) => {
+  const cardname = req.params.cardNumber
+  res.json(tarotData).then((tarotData)=>{
+    for (let index = 0; index < tarotData.length; index++) {
+      const tarot = tarotData[index];
+      console.log(tarot)
+      // if (tarot = cardname) {
+      //   return tarot
+      // } 
+      // res.json(tarot)
+    }
+  })
  
 })
+
+//I think we might have to add routes too the tarot keeper project im not completly sure how only one JSON would allow us to manipulate it into the actual project
+
+// app.get('/:id', (req, res) => {
+//   tarotData.findOne({
+//     where: {
+//       id: req.params.id,
+//     }
+//   })
+//     .then((data) => res.json(data))
+//     .catch((err) => res.status(400).json(err));
+// });
 
 
 app.listen(PORT, () => {
